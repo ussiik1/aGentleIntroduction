66b3042d7f13234e4c6f41a56fd557888fc505e3
Ushijima Koichi <ussiik001@gmail.com>
2015-05-09 08:07:15 +0900

    Routing Helpers

--- js/app.js
+++ js/app.js
@@ -4,8 +4,21 @@ ContactManager.addRegions({
   mainRegion: '#main-region'
 });
 
+ContactManager.navigate = function(route, options){
+  options = options || {};
+  Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function(){
+  return Backbone.history.fragment;
+};
+
 ContactManager.onStart = function(){
   if (Backbone.history) {
     Backbone.history.start();
-  }
+    if (Backbone.history.fragment === '') {
+      // URLを#contactsに変え, browserの履歴に追加する.
+      Backbone.history.navigate('contacts');
+      ContactManager.ContactsApp.List.Controller.listContacts();
+    }  }
 };
